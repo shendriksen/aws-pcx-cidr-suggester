c7ea5b56633ef408f036a464408500df634a832c
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-08-31 10:04:15 +0100

    feat: psudocode for implementation.

--- src/getAvailableCidrBlock.js
+++ src/getAvailableCidrBlock.js
@@ -1,12 +1,18 @@
-// import isCidrInRange from './cidr-functions/isCidrInRange';
+/* eslint-disable */
+import isCidrInRange from './cidr-functions/isCidrInRange';
 
-export default function getAvailableCidrBlock() {
-    //   newCidrBlockSize,
-    //   reservedIpRange,
-    //   occupiedCidrBlocks
-    //loop through occupied cidr blocks and filter out blocks outside the reserved space
-    //   const occupiedWithinRange = [];
-    //   occupiedCidrBlocks.forEach(block => {
-    //     isCidrInRange();
-    //   });
+export default function getAvailableCidrBlock(newCidrBlockSize, reservedIpRange, occupiedCidrBlocks) {
+
+    const availableAddress = reservedIpRange.startAddress;
+
+    // Sort the list.
+
+    occupiedCidrBlocks.forEach(block => {
+        //check if block is within range. isCidrInRange();
+        //if true then see if it clashes with our available address. (cidr-overlap)
+        //if true then move our available address to the end of the current block.
+        //if our new available range is within the reserved range then continue
+        //if the new available range is not then throw an error that there is no space.
+    });
+
 }
