3b7acb8433cac21749e9995932c35f0dbe12b925
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-09-03 16:38:44 +0100

    test: increased branch coverage.

--- src/cidr-functions/getAvailableCidrBlock.test.js
+++ src/cidr-functions/getAvailableCidrBlock.test.js
@@ -20,6 +20,25 @@ describe('getAvailableCidrBlock', () => {
         availableCidrBlock.should.equal('10.181.0.0/24');
     });
 
+    it('should return an available cidr block when there is space within the range', () => {
+        const newCidrBlockSize = 24;
+        const rangeStart = '10.180.0.0';
+        const rangeEnd = '10.200.0.0';
+        const occupiedCidrBlocks = [
+            '10.210.0.0/16',
+            '10.190.0.0/16',
+            '0.0.0.0/0'
+        ];
+
+        const availableCidrBlock = getAvailableCidrBlock(
+            newCidrBlockSize,
+            rangeStart, rangeEnd,
+            occupiedCidrBlocks
+        );
+
+        availableCidrBlock.should.equal('10.180.0.0/24');
+    });
+
     it('should return an error when there are no free CIDR blocks left', () => {
         const newCidrBlockSize = 24;
         const rangeStart = '10.180.0.0';
