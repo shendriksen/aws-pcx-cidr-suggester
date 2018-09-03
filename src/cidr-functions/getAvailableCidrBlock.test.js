0a4d94ef5623e4fd0e10ff83e08f1fae8d363b5d
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-09-03 11:29:38 +0100

    tests: fix failing tests.

--- src/cidr-functions/getAvailableCidrBlock.test.js
+++ src/cidr-functions/getAvailableCidrBlock.test.js
@@ -1,10 +1,10 @@
 import getAvailableCidrBlock from './getAvailableCidrBlock';
-import IpRange from './IpRange';
 
 describe('getAvailableCidrBlock', () => {
     it('should return an available cidr block when the reserved range is free', () => {
         const newCidrBlockSize = 24;
-        const reservedIpRange = new IpRange('10.180.0.0', '10.200.0.0');
+        const rangeStart = '10.180.0.0';
+        const rangeEnd = '10.200.0.0';
         const occupiedCidrBlocks = [
             '10.210.0.0/16',
             '10.180.0.0/16',
@@ -13,7 +13,7 @@ describe('getAvailableCidrBlock', () => {
 
         const availableCidrBlock = getAvailableCidrBlock(
             newCidrBlockSize,
-            reservedIpRange,
+            rangeStart, rangeEnd,
             occupiedCidrBlocks
         );
 
@@ -22,14 +22,15 @@ describe('getAvailableCidrBlock', () => {
 
     it('should return an error when there are no free CIDR blocks left', () => {
         const newCidrBlockSize = 24;
-        const reservedIpRange = new IpRange('10.180.0.0', '10.181.0.0');
+        const rangeStart = '10.180.0.0';
+        const rangeEnd = '10.181.0.0';
         const occupiedCidrBlocks = ['10.180.0.0/16'];
         let expectedError;
 
         try {
             getAvailableCidrBlock(
                 newCidrBlockSize,
-                reservedIpRange,
+                rangeStart, rangeEnd,
                 occupiedCidrBlocks
             );
         } catch (error) {
